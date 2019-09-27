fe01378cb0e3602be703d05ce64b4772b11fc71e
Aakash kumar <aakash012@live.com>
2019-09-27 19:36:28 +0530

    App.js initialised

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import Hero from './components/Hero/Hero';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
+import { PortfolioProvider } from './context';
+
+import{
+    heroData,
+    aboutData,
+    projectsData,
+    contactData,
+    footerData,
+}   from './data';
+
+function App(){
+    const [hero, setHero] = useState({});
+    const [about, setAbout] = useState({});
+    const [projects, setProjects] = useState({});
+    const [contact, setContact] = useState({});
+    const [footer, setFooter] = useState({});
+
+    useEffect(() => {
+        setHero({ ...heroData});
+        setAbout({ ...aboutData});
+        setProjects({ ...projectsData});
+        setContact({ ...projectsData});
+        setFooter({ ...footerData});
+    }, []);
+
+    return(
+        <PortfolioProvider value={{hero, about, projects, contact, footer}}>
+            <Hero />
+            <About />
+            <Projects />
+            <Contact />
+            <Footer />
+        </PortfolioProvider>
+    );
+}
+
+export default App;
