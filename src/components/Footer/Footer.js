05bcbb9b687fefcef673b49fd661e84ece4e63e7
Aakash kumar <aakash012@live.com>
2019-09-27 21:57:00 +0530

    Footer Component added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import PortfolioContext from '../../context';
+
+const Footer = () => {
+  const { footer } = useContext(PortfolioContext);
+  const { networks } = footer;
+
+  console.log(networks);
+
+  return (
+    <footer className="footer navbar-static-bottom">
+      <div className="container">
+        <a href="#top" className="back-to-top">
+          <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
+        </a>
+
+        <div className="social-links">
+          {networks &&
+            networks.map((network, index) => {
+              const { name, url } = network;
+              return (
+                <a
+                  key={index}
+                  href={url || 'https://github.com/cobidev/react-simplefolio'}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
+                </a>
+              );
+            })}
+        </div>
+
+        <hr />
+
+        <p className="footer__text">
+          © 2019 - Template by{' '}
+          <a
+            href="https://github.com/cobidev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jacobo Martinez
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
