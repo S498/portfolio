93e66baa13fdf980fb7b41407f73db22717b25b7
Aakash kumar <aakash012@live.com>
2019-09-27 22:30:00 +0530

    Components created & Styling starts

---
+++ src/data.js
@@ -0,0 +1,69 @@
+// HERO DATA
+export const heroData = {
+  title: '',
+  name: '',
+  subtitle: '',
+  cta: '',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: '',
+  paragraphOne: '',
+  paragraphTwo: '',
+  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    title: '',
+    info: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: '',
+  },
+  {
+    title: '',
+    info: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: '',
+  },
+  {
+    title: '',
+    info: '',
+    url: '',
+    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: '',
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '',
+  btn: '',
+  email: '',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      name: 'twitter',
+      url: '',
+    },
+    {
+      name: 'codepen',
+      url: '',
+    },
+    {
+      name: 'linkedin',
+      url: '',
+    },
+    {
+      name: 'github',
+      url: '',
+    },
+  ],
+};
